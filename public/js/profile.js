2e2349923dbd64c7e81d4ffcc75ed1699b9fd382
Felipe Cristino <felipebcristino@gmail.com>
2026-09-23 04:27:44 -0400

    trigesimo oitavo

--- public/js/profile.js
+++ public/js/profile.js
@@ -13,15 +13,6 @@ const usernameInputGroup = usernameForm.querySelector(".input-group")
 const submitUsernameChangeBtn = document.getElementById("submit-username-change");
 // --------------------------------------------------------
 
-// - Email elements
-const changeEmailBtn = document.getElementById("change-email-btn")
-const emailInput = document.getElementById("email")
-const emailForm = document.getElementById("email-form")
-const emailFormSubmitBtn = emailForm.querySelector("button.hidden")
-const emailInputGroup = emailForm.querySelector(".input-group")
-const submitEmailChangeBtn = document.getElementById("submit-email-change");
-// --------------------------------------------------------
-
 // - Password elements
 const changePasswordBtn = document.getElementById("change-password-btn")
 const cancelPasswordChange = document.getElementById("cancel-password-change");
@@ -30,15 +21,11 @@ const oldPasswordInput = document.getElementById("old-password")
 const newPasswordInput = document.getElementById("new-password")
 const submitPasswordChangeBtn = document.getElementById("submit-password-change")
 // --------------------------------------------------------
-
 usernameInput.readOnly = true;
-emailInput.readOnly = true;
-
 // =========================================================
 
 // Variables
 let isAboutToChangeUsername = false;
-let isAboutToChangeEmail = false;
 let isAboutToChangePassword = false;
 
 let user;
@@ -50,7 +37,6 @@ const fetchUserCallback = (data) => {
     socket.emit('user-connected', user);
 
     usernameInput.value = user.username;
-    emailInput.value = user.email;
     profile.classList.remove("hidden");
 
     hideSpinner()
@@ -72,8 +58,6 @@ const submitForm = (url, body) => {
 
         if(body.username){
             user.username = body.username;
-        }else if(body.email){
-            user.email = body.email;
         }
 
         setToastType('success');
@@ -88,11 +72,8 @@ const submitForm = (url, body) => {
             hideChangeUsername();
             submitUsernameChangeBtn.disabled = false;
             submitUsernameChangeBtn.classList.remove("disabled");
-        }else if(isAboutToChangeEmail){
-            hideChangeEmail();
-            submitEmailChangeBtn.disabled = false;
-            submitEmailChangeBtn.classList.remove("disabled");
-        }else{
+        }
+        else{
             hideChangePassword();
             oldPasswordInput.value = ""
             newPasswordInput.value = ""
@@ -109,15 +90,6 @@ const handleUsernameSubmit = (e) => {
     submitUsernameChangeBtn.classList.add('disabled');
 
     submitForm(`/api/user/username/${user.id}`, {username: usernameInput.value})
-}
-
-const handleEmailSubmit = (e) => {
-    e.preventDefault();
-
-    submitEmailChangeBtn.disabled = true;
-    submitEmailChangeBtn.classList.add('disabled');
-
-    submitForm(`/api/user/email/${user.id}`, {email: emailInput.value})
 }
 
 const handlePasswordSubmit = (e) => {
@@ -151,30 +123,7 @@ const hideChangeUsername = () => {
     usernameInput.value = user.username
 }
 
-const showChangeEmail = () => {
-    changeEmailBtn.innerText = "Cancel";
-    changeEmailBtn.classList.add("cancel");
-    emailFormSubmitBtn.classList.remove("hidden");
-    emailInputGroup.classList.remove("disabled");
-    isAboutToChangeEmail = true;
-    emailInput.readOnly = false;
-}
-
-const hideChangeEmail = () => {
-    changeEmailBtn.innerText = "Change";
-    changeEmailBtn.classList.remove("cancel");
-    emailFormSubmitBtn.classList.add("hidden");
-    emailInputGroup.classList.add("disabled");
-    isAboutToChangeEmail = false;
-    emailInput.readOnly = true;
-    emailInput.value = user.email
-}
-
 const showChangePassword = () => {
-    if(isAboutToChangeEmail){
-        hideChangeEmail()
-    }
-
     if(isAboutToChangeUsername){
         hideChangeUsername()
     }
@@ -195,10 +144,6 @@ const hideChangePassword = () => {
 fetchData('/api/user-info', fetchUserCallback)
 
 changeUsernameBtn.addEventListener("click", () => {
-    if(isAboutToChangeEmail){
-        hideChangeEmail()
-    }
-
     if(isAboutToChangePassword){
         hideChangePassword()
     }
@@ -207,22 +152,6 @@ changeUsernameBtn.addEventListener("click", () => {
         hideChangeUsername()
     }else{
         showChangeUsername()
-    }
-})
-
-changeEmailBtn.addEventListener("click", () => {
-    if(isAboutToChangeUsername){
-        hideChangeUsername()
-    }
-
-    if(isAboutToChangePassword){
-        hideChangePassword()
-    }
-
-    if(isAboutToChangeEmail){
-        hideChangeEmail()
-    }else{
-        showChangeEmail()
     }
 })
 
@@ -246,7 +175,4 @@ deleteAccountBtn.addEventListener("click", async () => {
 })
 
 usernameForm.addEventListener("submit", handleUsernameSubmit)
-
-emailForm.addEventListener("submit", handleEmailSubmit);
-
 passwordForm.addEventListener("submit", handlePasswordSubmit)
